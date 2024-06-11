3fa51bbb196158305391f70ce0af229ca4a840c0
shrish <mynameisshrish@gmail.com>
2024-06-11 18:12:41 +0530

    Front End

---
+++ CoffeeStoreAPI/ServiceTests/OrderServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using CoffeeStoreAPI.Context;
+using CoffeeStoreAPI.Iterfaces;
+using CoffeeStoreAPI.Models;
+using CoffeeStoreAPI.Repositories;
+using CoffeeStoreAPI.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceTests
+{
+    public class OrderServiceTests
+    {
+        private CoffeeStoreContext context;
+
+        private IRepository<int, Item> itemRepository;
+        private IRepository<int, ItemType> itemTypeRepository;
+        private IItemServices itemServices;
+
+        [SetUp]
+        public void SetUp()
+        {
+            DbContextOptionsBuilder optionsBuilder = new DbContextOptionsBuilder().UseInMemoryDatabase("dummyDB");
+            context = new CoffeeStoreContext(optionsBuilder.Options);
+
+            itemRepository = new ItemRepository(context);
+            itemTypeRepository = new ItemTypeRepository(context);
+            itemServices = new ItemServices(itemRepository, itemTypeRepository);
+        }
+
+    }
+}
--- CoffeeStoreAPI/ServiceTests/obj/Debug/net6.0/ServiceTests.AssemblyInfo.cs
+++ CoffeeStoreAPI/ServiceTests/obj/Debug/net6.0/ServiceTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52c5f02f36a65073e02553e598db29413c2defed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a828154fc0e9cd91492743e8dde5b595f24a544")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
